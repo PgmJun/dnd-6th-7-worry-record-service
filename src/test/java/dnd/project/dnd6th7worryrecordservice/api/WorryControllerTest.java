4aa7fa602d77be930e0a2f71d1e7f1cebc173aa1
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-11 22:54:37 +0900

    test: WorryController home api 테스트 #201

--- src/test/java/dnd/project/dnd6th7worryrecordservice/api/WorryControllerTest.java
+++ src/test/java/dnd/project/dnd6th7worryrecordservice/api/WorryControllerTest.java
@@ -1,34 +1,64 @@
 package dnd.project.dnd6th7worryrecordservice.api;
 
+import dnd.project.dnd6th7worryrecordservice.api.HealthCheck;
+import dnd.project.dnd6th7worryrecordservice.config.SecurityConfig;
+import dnd.project.dnd6th7worryrecordservice.config.WebConfig;
+import dnd.project.dnd6th7worryrecordservice.jwt.JwtUtil;
+import dnd.project.dnd6th7worryrecordservice.security.filter.JwtAuthenticationFilter;
 import dnd.project.dnd6th7worryrecordservice.service.WorryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.context.WebApplicationContext;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(WorryController.class)
-@DisplayName("MemberController 테스트")
-public class WorryControllerTest {
+import java.nio.charset.StandardCharsets;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(value = WorryController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebConfig.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebConfig.class)
+        })
+class WorryControllerTest {
+
+    @Autowired
     private MockMvc mvc;
 
     @MockBean
     private WorryService worryService;
 
-    @BeforeEach
-    public void setUp() {
-        mvc =
-                MockMvcBuilders.standaloneSetup(new WorryController(worryService))
-                        .addFilters(new CharacterEncodingFilter("UTF-8", true)) // utf-8 필터 추가
-                        .build();
+    @Test
+    @WithMockUser(roles = "USER")
+    @DisplayName("Home API 테스트")
+    void homeTest() throws Exception {
+        //given
+        String value = "1";
+
+        //when
+        final ResultActions actions = mvc.perform(get("/worries/home")
+                .param("userId", value));
+
+        //then
+        actions.andDo(print())
+                .andExpect(status().isOk());
     }
-
 }
-
